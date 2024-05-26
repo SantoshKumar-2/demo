d10bde709f65694b8df7233e65566fe481e3757f
Santosh Kumar K <157588967+SantoshKumar-2@users.noreply.github.com>
2024-05-26 12:45:42 +0530

    Add files via upload

---
+++ sudoku.java
@@ -0,0 +1,89 @@
+public class sudoku {
+    //check the condition
+    public static boolean issafe(int sudoku[][],int row,int col,int digit){
+        //check in column
+        for(int i=0;i<=8;i++){
+            if(sudoku[i][col]==digit){
+                return false;
+            }
+        }
+        //check the row wise
+        for(int j=0;j<=8;j++){
+            if(sudoku[row][j]==digit){
+                return false;
+            }
+        }
+        //gride
+        //formula for finding starting row and column in 3*3 gride
+        int sr=(row/3)*3;
+        int sc=(col/3)*3;
+        //check the 3*3 gride
+        for(int i=sr;i<sr+3;i++){
+            for(int j=sc;j<sc+3;j++){
+                if(sudoku[i][j]==digit){
+                    return false;
+                }
+            }
+        } 
+        return true;
+    }
+//sudoku solver main code 
+public static boolean sudokusolver(int sudoku[][],int row,int col){
+    //base case
+    if(row==9){
+        return true;
+    }
+    //recursion
+    int nextrow=row,nextcol=col+1;
+    if(col+1==9){
+        nextrow=row+1;
+        nextcol=0;
+    }
+    //if number is present
+    if(sudoku[row][col]!=0){
+        return sudokusolver(sudoku, nextrow, nextcol);
+    }
+    //placing the digit in 0 valued place
+    for(int digit=1;digit<=9;digit++){
+        if(issafe(sudoku,row,col,digit)){        
+            sudoku[row][col]=digit;
+        
+if(sudokusolver(sudoku, nextrow, nextcol)){//sol exist
+            return true;
+        }
+        sudoku[row][col]=0;
+    }
+}
+return false;
+}
+//print the sudoku
+public static void printsudoku(int sudoku[][]){
+    for(int i=0; i<9;i++){
+        for(int j=0;j<9;j++){
+            System.out.print(sudoku[i][j]+" ");
+        }
+    System.out.println();
+    }
+}
+
+    public static void main(String args[]){
+int sudoku[][]={{0,0,8,0,0,0,0,0,0},
+{4,9,0,1,5,7,0,0,2},
+{0,0,3,0,0,4,1,9,0},
+{1,8,5,0,6,0,0,2,0},
+{0,0,0,0,2,0,0,6,0},
+{9,6,0,4,0,5,3,0,0},
+{0,3,0,0,7,2,0,0,4},
+{0,4,9,0,3,0,0,5,7},
+{8,2,7,0,0,9,0,1,3}
+};
+//function call
+if(sudokusolver(sudoku, 0, 0)){
+    System.out.println("solution exits");
+    printsudoku(sudoku);
+}else{
+    System.out.println("solution not exits");
+}
+    }
+    
+}
